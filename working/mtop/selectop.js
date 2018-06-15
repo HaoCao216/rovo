78d69462aac78e3032620f032da68cd7f7ec9345
Ti <apologize216@gmail.com>
2018-06-16 06:09:22 +0700

    login-layout

---
+++ working/mtop/selectop.js
@@ -0,0 +1,18 @@
+var $disabledResults = $(".js-example-disabled-results");
+$disabledResults.select2();
+
+var data1 = [
+   
+];
+
+var data2 = [
+
+];
+
+$(".city").select2({
+  data: data1
+})
+
+$(".country").select2({
+  data: data2
+})
